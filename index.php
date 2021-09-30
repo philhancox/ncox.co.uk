c8acfc01dfa3c2ed162e203cb98ec7c87293f7a2
Phil Hancox <philhancox@gmail.com>
2021-09-30 18:33:58 +0100

    Init

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ncox.co.uk, the second home of Phil Hancox</title>
+
+    <meta name="description" content="Have you seen my email address and wondered what is ncox.co.uk? Well, this is it! :)">
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/d4b9b706f5.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" type="image/png" href="/phavicon.png" />
+    <link rel="stylesheet" href="/paint.css">
+</head>
+<body>
+<div class="container-fluid container-table">
+    <div class="row vertical-center-row">
+        <div id="ncox" class="col-md-offset-2 col-md-8 col-xs-12">
+            <p><img src="/phil-hancox.png" class="img-fluid" alt="Profile image of Phil Hancox">
+                👋 Hello! My name is <strong class="emphasis">Phil Hancox</strong>.
+                I'm a web developer based in Sheffield, England and yes, I purchased <strong class="emphasis">ncox.co.uk</strong> for the sole purpose of having the email address <strong class="emphasis">philh@ncox.co.uk</strong>.
+                <br><strong>Have a great day!</strong>
+            </p>
+            <p class="icons">
+                <a href="http://www.philhancox.co.uk" target="_blank" rel="noopener noreferrer"><span class="fas fa-globe"></span></a>
+                <a href="https://www.linkedin.com/in/philhancox/" target="_blank" rel="noopener noreferrer"><span class="fab fa-linkedin"></span></a>
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
